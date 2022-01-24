0611da1aff4f78cc61073f09a67b77088655b284
Zail <zzzzail.mail@gmail.com>
2022-01-24 14:00:07 +0800

    打包

--- src/main/java/com/fsocity/security/component/DynamicAccessDecisionManager.java
+++ src/main/java/com/fsocity/security/component/DynamicAccessDecisionManager.java
@@ -18,7 +18,8 @@ import java.util.Iterator;
 public class DynamicAccessDecisionManager implements AccessDecisionManager {
 
     @Override
-    public void decide(Authentication authentication, Object object,
+    public void decide(Authentication authentication,
+                       Object object,
                        Collection<ConfigAttribute> configAttributes) throws AccessDeniedException, InsufficientAuthenticationException {
         // 当接口未被配置资源时直接放行
         if (CollUtil.isEmpty(configAttributes)) {
