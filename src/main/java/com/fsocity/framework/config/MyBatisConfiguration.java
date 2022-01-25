1b2b2b9e3fd6ffd6560a3c335c8df91cd061cd59
Zail <zzzzail.mail@gmail.com>
2022-01-25 09:55:02 +0800

    1

--- src/main/java/com/fsocity/framework/config/MyBatisConfiguration.java
+++ src/main/java/com/fsocity/framework/config/MyBatisConfiguration.java
@@ -1,9 +1,6 @@
 package com.fsocity.framework.config;
 
-import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -15,12 +12,5 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @MapperScan({"com.fsocity.modules.*.mapper"})
 public class MyBatisConfiguration {
-    
-    @Bean
-    public PaginationInterceptor paginationInterceptor() {
-        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
-        paginationInterceptor.setCountSqlParser(new JsqlParserCountOptimize(true));
-        return paginationInterceptor;
-    }
     
 }
